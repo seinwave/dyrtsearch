3a9a8d27dfecabe8daa9d8ba1a74517dacc74d38
Matt Seidholz <mseidholz@gmail.com>
2021-07-21 11:14:41 -0400

    Updates

--- src/components/ErrorFallback/ErrorFallback.tsx
+++
@@ -1,9 +0,0 @@
-export const ErrorFallback = (error: any, resetErrorBoundary: any) => {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  );
-};
